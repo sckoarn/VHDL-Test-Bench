9247ce373321142227cabef73f7a3164bf740eaa
sckoarn <sckoarn@storm.ca>
2023-04-11 20:04:42 -0400

    example1
    Initial addition of Example1

---
+++ examples/example1/vhdl/example_dut_ttb_ent.vhd
@@ -0,0 +1,12 @@
+
+library IEEE;
+--library dut_lib;
+use IEEE.STD_LOGIC_1164.all;
+--use dut_lib.all;
+use work.all;
+
+entity example_dut_ttb is
+  generic (
+           stimulus_file: string := "stm/stimulus_file.stm"
+          );
+end example_dut_ttb;
